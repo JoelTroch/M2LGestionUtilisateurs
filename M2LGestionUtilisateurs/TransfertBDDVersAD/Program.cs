631e0e876ffef36ec9af7e029d132737dc91b4cb
Joël Troch <joel.troch62@gmail.com>
2015-03-09 16:20:10 +0100

    [TransfertBDDVersAD] Connexion à la base de données

--- M2LGestionUtilisateurs/TransfertBDDVersAD/Program.cs
+++ M2LGestionUtilisateurs/TransfertBDDVersAD/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace TransfertBDDVersAD
 {
@@ -6,6 +10,24 @@ namespace TransfertBDDVersAD
     {
         static void Main()
         {
+            // Lecture de la configuration
+            string repertoireActuel = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            Config configuration = new Config(repertoireActuel + "\\config.ini");
+
+            // Connection base de données
+            MySqlConnection bddConnection = new MySqlConnection("SERVER=" + configuration.lire("host", "localhost") +
+                ";DATABASE=" + configuration.lire("database", "mrbs") + ";UID=" + configuration.lire("user", "mrbs") +
+                ";PASSWORD=" + configuration.lire("password", "mrbs"));
+            try
+            {
+                bddConnection.Open();
+
+                bddConnection.Close();
+            }
+            catch (Exception erreur)
+            {
+                MessageBox.Show(erreur.Message, "Erreur lors de la connection à la base de données", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
