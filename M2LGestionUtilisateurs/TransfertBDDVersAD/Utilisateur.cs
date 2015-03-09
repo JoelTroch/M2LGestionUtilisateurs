0978789cf8eeb5954c18befd76b7a968f79fe9ce
Joël Troch <joel.troch62@gmail.com>
2015-03-09 16:51:17 +0100

    [TransfertBDDVersAD] Ajout de la classe "Utilisateur"

---
+++ M2LGestionUtilisateurs/TransfertBDDVersAD/Utilisateur.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TransfertBDDVersAD
+{
+    public class Utilisateur
+    {
+        // Attributs
+        private int id;
+        private int niveau;
+        private string nom;
+        private string motDePasse;
+        private string email;
+
+        // Constructeur
+        public Utilisateur(int id, int niveau, string nom, string motDePasse, string email)
+        {
+            this.id = id;
+            this.niveau = niveau;
+            this.nom = nom;
+            this.motDePasse = motDePasse;
+            this.email = email;
+        }
+    }
+}
