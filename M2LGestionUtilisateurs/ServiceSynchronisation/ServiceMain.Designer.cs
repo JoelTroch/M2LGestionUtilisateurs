16910db432817a8ea7ca56e0e92546ff16bc89ca
Joël Troch <joel.troch62@gmail.com>
2015-03-30 14:33:38 +0200

    [ServiceSynchronisation] Début de code LDAP

--- M2LGestionUtilisateurs/ServiceSynchronisation/ServiceMain.Designer.cs
+++ M2LGestionUtilisateurs/ServiceSynchronisation/ServiceMain.Designer.cs
@@ -28,10 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            // 
+            // ServiceMain
+            // 
             this.ServiceName = "Service1";
+
         }
 
         #endregion
+
+        private System.Diagnostics.EventLog logger;
     }
 }
--- M2LGestionUtilisateurs/ServiceSynchronisation/ServiceMain.cs
+++ M2LGestionUtilisateurs/ServiceSynchronisation/ServiceMain.cs
@@ -1,23 +1,64 @@
 ﻿using System;
+using System.Diagnostics;
+using System.DirectoryServices;
+using System.IO;
+using System.Reflection;
 using System.ServiceProcess;
-using System.Text;
+using System.Timers;
 
 namespace ServiceSynchronisation
 {
     public partial class ServiceMain : ServiceBase
     {
+        private Config configuration = null;
+
         public ServiceMain()
         {
             InitializeComponent();
+            this.AutoLog = false;
+            logger = new EventLog();
+            if (!EventLog.SourceExists("ServiceSynchronisation"))
+                EventLog.CreateEventSource("ServiceSynchronisation", "ServiceSynchronisationLog");
+
+            logger.Source = "ServiceSynchronisation";
+            logger.Log = "ServiceSynchronisationLog";
         }
 
         protected override void OnStart(string[] args)
         {
+            logger.WriteEntry("Démarrage, date UTC = " + DateTime.UtcNow.ToString(), EventLogEntryType.Information);
             base.OnStart(args);
+
+            // Lecture de la configuration
+            string repertoireActuel = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            configuration = new Config(repertoireActuel + "\\config.ini");
+
+            // Prépare le timer
+            Timer compteur = new Timer();
+            compteur.Interval = double.Parse(configuration.lire("service", "intervalle", "30"));
+            compteur.Elapsed += new ElapsedEventHandler(EffectueMAJ);
+            compteur.Start();
+        }
+
+        public void EffectueMAJ(object sender, ElapsedEventArgs args)
+        {
+            if (configuration != null)
+            {
+                // Connexion au LDAP
+                DirectoryEntry ldapServeur = new DirectoryEntry("LDAP://" + configuration.lire("ldap", "host", "169.254.36.173") + "/OU=usersM2L,DC=m2l,DC=fr", configuration.lire("ldap", "user", "Administrateur"), configuration.lire("ldap", "password", "Thoughtpolice2008"));
+
+                // Récupération de tous les utilisateurs dans le LDAP
+                DirectorySearcher ldapRecherche = new DirectorySearcher(ldapServeur);
+                ldapRecherche.Filter = "(objectClass=user)";
+                SearchResultCollection ldapResultat = ldapRecherche.FindAll();
+            }
+            else
+                logger.WriteEntry("Pas de pointeur sur la classe configuration !", EventLogEntryType.Error);
         }
 
         protected override void OnStop()
         {
+            logger.WriteEntry("Arrêt, date UTC = " + DateTime.UtcNow.ToString(), EventLogEntryType.Information);
             base.Stop();
         }
     }
