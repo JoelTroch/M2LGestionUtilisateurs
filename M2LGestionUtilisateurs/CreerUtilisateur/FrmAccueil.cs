c97630f81b933afa50b448e0ea62f583b907c4e6
Joël Troch <joel.troch62@gmail.com>
2015-03-30 14:10:28 +0200

    Optimisation des using

--- M2LGestionUtilisateurs/CreerUtilisateur/FrmAccueil.cs
+++ M2LGestionUtilisateurs/CreerUtilisateur/FrmAccueil.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
 using System.DirectoryServices;
+using System.Drawing;
 using System.IO;
 using System.Reflection;
+using System.Windows.Forms;
 
 namespace CreerUtilisateur
 {
